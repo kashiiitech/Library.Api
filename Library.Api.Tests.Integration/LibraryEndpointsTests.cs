601af774dd1956aff4ea71682bd0556c02765e7a
kashiiitech <kashiiitech@gmail.com>
2025-01-13 14:49:56 +0500

    Create Book test cases added

--- Library.Api.Tests.Integration/LibraryEndpointsTests.cs
+++ Library.Api.Tests.Integration/LibraryEndpointsTests.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
+﻿using System.Net;
+using FluentAssertions;
+using Library.Api.Models;
+using Microsoft.AspNetCore.Mvc.Testing;
 
 namespace Library.Api.Tests.Integration
 {
@@ -12,9 +15,78 @@ namespace Library.Api.Tests.Integration
         }
 
         [Fact]
-        public void Test()
+        public async Task CreateBook_CreatesBook_WhenDataIsCorrect()
         {
+            // Arrage
             var httpClient = _factory.CreateClient();
+            var book = GenerateBook();
+
+            // Act
+            var result = await httpClient.PostAsJsonAsync("/books", book);
+            var createdBook = await result.Content.ReadFromJsonAsync<Book>();
+
+            // Assert
+            result.StatusCode.Should().Be(HttpStatusCode.Created);
+            createdBook.Should().BeEquivalentTo(book);
+            result.Headers.Location.Should().Be($"/books/{book.Isbn}");
+        }
+
+        [Fact]
+        public async Task CreateBook_Fails_WhenIsbnIsInvalid()
+        {
+            // Arrage
+            var httpClient = _factory.CreateClient();
+            var book = GenerateBook();
+            book.Isbn = "INVALID";
+
+            // Act
+            var result = await httpClient.PostAsJsonAsync("/books", book);
+            var errors = await result.Content.ReadFromJsonAsync<IEnumerable<ValidationError>>();
+            var error = errors!.Single();
+
+            // Assert
+            result.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+            error.PropertyName.Should().Be("Isbn");
+            error.ErrorMessage.Should().Be("Value was not a valid ISBN-13");
+        }
+
+
+        [Fact]
+        public async Task CreateBook_Fails_WhenBookExists()
+        {
+            // Arrage
+            var httpClient = _factory.CreateClient();
+            var book = GenerateBook();
+
+            // Act
+            await httpClient.PostAsJsonAsync("/books", book);
+            var result = await httpClient.PostAsJsonAsync("/books", book);
+            var errors = await result.Content.ReadFromJsonAsync<IEnumerable<ValidationError>>();
+            var error = errors!.Single();
+
+            // Assert
+            result.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+            error.PropertyName.Should().Be("Isbn");
+            error.ErrorMessage.Should().Be("A book with this ISBN-13 already exists!");
+        }
+
+        private Book GenerateBook(string title = "The Dirty Coder")
+        {
+            return new Book
+            {
+                Isbn = GenerateIsbn(),
+                Title = title,
+                Author = "Kashif Ali",
+                PageCount = 420,
+                ShortDescription = "All my tricks to learn minimal api in one book",
+                ReleaseDate = new DateTime(2023, 1, 1),
+            };
+        }
+
+        private string GenerateIsbn()
+        {
+            return $"{Random.Shared.Next(100, 999)}-" +
+                   $"{Random.Shared.Next(1000000000, 2100999999)}";
         }
     }
 }
