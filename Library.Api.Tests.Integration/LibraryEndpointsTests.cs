ad83438a2cd3b8cc2b5c1a12c52aa07117611133
kashiiitech <kashiiitech@gmail.com>
2025-01-13 13:41:50 +0500

    added integration testing

---
+++ Library.Api.Tests.Integration/LibraryEndpointsTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Library.Api.Tests.Integration
+{
+    public class LibraryEndpointsTests : IClassFixture<WebApplicationFactory<IApiMarker>>
+    {
+        private readonly WebApplicationFactory<IApiMarker> _factory;
+
+        public LibraryEndpointsTests(WebApplicationFactory<IApiMarker> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public void Test()
+        {
+            var httpClient = _factory.CreateClient();
+        }
+    }
+}
